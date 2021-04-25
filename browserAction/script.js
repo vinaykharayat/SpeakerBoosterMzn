ac88da5f2f78d0d41a0a8abfbbe0545af6aeff06
vinaykharayat <vinaykharayat@gmail.com>
2021-04-25 15:57:37 +0530

    fixed storage issue

--- browserAction/script.js
+++ browserAction/script.js
@@ -2,22 +2,32 @@ let sliderNode = document.getElementById("volumeSlider");
 let body = document.querySelector("body");
 let volumneValue = document.createElement("label");
 body.append(volumneValue);
-
+volumneValue.innerText = sliderNode.value + "%";
 sliderNode.addEventListener("change", e => setValue(e.target.value));
 
 async function setValue(value){
 	await browser.storage.local.set({value});
+	volumneValue.innerText = value *10 + "%";
+	let value1 = browser.storage.local.get("value");
+	value1.then((value)=>{
+		volumneValue.innerText = value.value *10 + "%";
+	});
+
 }
 
 async function init(){
-	let {value} = browser.local.storage.get("value");
+	let value = browser.storage.local.get("value");
 	if(!value){
 		value = 0;
 	}
-	sliderNode.value = value;
-	setValue(value);
-}
+	value.then((value)=>{
+		sliderNode.value = value.value;
+		setValue(value.value);
 
+	});
+	// sliderNode.value = value;
+	// setValue(value);
+}
 init().catch(e=> console.error(e));
 
 
