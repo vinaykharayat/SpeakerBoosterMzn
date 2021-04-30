bec263b6d3412600c053daa173a03db9d075018c
vinaykharayat <vinaykharayat@gmail.com>
2021-04-30 18:45:06 +0530

    "Added enable disable toggle and some other improvements"

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,31 +1,56 @@
 let sliderNode = document.getElementById("volumeSlider");
 let body = document.querySelector("body");
-
-let volumneValue = document.createElement("label");
+let volumneValue = document.getElementById("volumePercentage");
 let mVideoElement = document.querySelector('video');
-body.append(volumneValue);
+let mEnableSpeakerBooster = document.querySelector("#enableSpeakerBooster");
+
 volumneValue.innerText = sliderNode.value + "%";
+
 sliderNode.addEventListener("change", e => setValue(e.target.value));
+mEnableSpeakerBooster.addEventListener("change", e => setStatus(e.target.checked));
 
-async function setValue(value){
-	await browser.storage.local.set({value});
-	volumneValue.innerText = value *10 + "%";
-	let value1 = browser.storage.local.get("value");
-	value1.then((value)=>{
-		volumneValue.innerText = value.value *10 + "%";
-	});
+async function setStatus(status) {
+    await browser.storage.local.set({ status });
+    let statusValue = browser.storage.local.get("status");
+    statusValue.then((value) => {
+        mEnableSpeakerBooster.checked = value.status;
+    });
+    if (!mEnableSpeakerBooster.checked) {
+        sliderNode.disabled = true;
+    } else {
+        sliderNode.disabled = false;
+
+    }
 }
 
-async function init(){
-	let value = browser.storage.local.get("value");
-	value.then((value)=>{
-		if(!value.value){
-			value.value = mVideoElement.volume;
-			console.log(value.value);
-		}
-		sliderNode.value = value.value;
-		setValue(value.value);
-
-	});
+async function setValue(value) {
+    await browser.storage.local.set({ value });
+    volumneValue.innerText = value * 10 + "%";
+    let value1 = browser.storage.local.get("value");
+    value1.then((value) => {
+        volumneValue.innerText = value.value * 10 + "%";
+    });
 }
-init().catch(e=> console.error(e));
+
+async function init() {
+    let value = browser.storage.local.get("value");
+    let status = browser.storage.local.get("status");
+
+    status.then((value) => {
+        if (!value.status) {
+            value.status = false;
+        }
+        mEnableSpeakerBooster.checked = value.status;
+        setStatus(value.status);
+    });
+
+    value.then((value) => {
+        if (!value.value) {
+            value.value = mVideoElement.volume;
+        }
+        sliderNode.value = value.value;
+        setValue(value.value);
+
+    });
+}
+init().catch(e => console.error(e));
--- content_script.js
+++ content_script.js
@@ -1,17 +1,36 @@
-browser.storage.onChanged.addListener((changes, area) =>{
-	if(area === 'local' && 'value' in changes){
-		increaseVolume(changes.value.newValue);
-	}
-});
 let mVideoElement = document.querySelector('video');
 let audioCtx = new AudioContext();
 let source = audioCtx.createMediaElementSource(mVideoElement);
 let gainNode = audioCtx.createGain();
 
-function increaseVolume(value){
-	gainNode.gain.value = value;
-	source.connect(gainNode);
-	gainNode.connect(audioCtx.destination);
+browser.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local' && 'value' in changes) {
+        increaseVolume(changes.value.newValue);
+    }
+
+    if (area === 'local' && 'status' in changes) {
+        speakerBoosterStatus(changes.status.newValue);
+    }
+});
+
+function increaseVolume(value) {
+    if (value === undefined) {
+        gainNode.gain.value = 0;
+    } else {
+        gainNode.gain.value = value;
+    }
+    source.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
 }
 
-browser.storage.local.get('value').then(result=> increaseVolume(result.value));
+
+function speakerBoosterStatus(status) {
+    if (status === false) {
+        gainNode.gain.value = 0;
+    } else {
+        browser.storage.local.get('value').then(result => increaseVolume(result.value));
+    }
+}
+
+browser.storage.local.get('value').then(result => increaseVolume(result.value));
+browser.storage.local.get('status').then(result => speakerBoosterStatus(result.value));
